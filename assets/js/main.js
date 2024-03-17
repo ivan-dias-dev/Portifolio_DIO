91ff75c69f2659a4c303fa992f9a83c88983e36c
ivan-dias-dev <mangakaivan@gmail.com>
2024-03-16 22:11:37 -0300

    ass lgg

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ function updateProfaleInfo(profileData) {
   let photo = document.getElementById("profile.photo");
   photo.src = profileData.photo;
   photo.alt = profileData.name;
-  console.log(profileData, photo);
 
   let name = document.getElementById("profile.name");
   name.innerText = profileData.name;
@@ -17,24 +16,30 @@ function updateProfaleInfo(profileData) {
   let email = document.getElementById("profile.email");
   email.innerText = profileData.email;
   email.href = `mailto:${profileData.email}`;
-  console.log(name, job, location, phone, email);
   
 }
 
 
 function updateSokftSkills(profileData){
     let softSkills = document.getElementById("profile.skills.softSkills");
-    console.log(profileData.skills.softSkills)
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join(' ')
     
 }
 function updatehardtSkills(profileData){
     let hardSkills = document.getElementById("profile.skills.hardSkills");
-    console.log(profileData.skills.hardSkills)
+
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `
     <li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"/></li>
     `).join(' ')
   
+}
+function updateLanguages(profileData){
+  let languages = document.getElementById("languages");
+  console.log(profileData.languages)
+  languages.innerHTML = profileData.languages.map(language => `
+  <li>${language}</li>
+  `).join(' ')
+
 }
 
 (async function () {
@@ -42,4 +47,5 @@ function updatehardtSkills(profileData){
   updateProfaleInfo(profileData);
   updateSokftSkills(profileData)
   updatehardtSkills(profileData)
+  updateLanguages(profileData)
 })();
